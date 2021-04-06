915cdfc4856c83ec1309e2f7c22598414044e6dc
Kostya Shavkunov <purposef@yandex.ru>
2021-04-06 13:07:23 +0500

    refactor(): change the run.sh for sql server

---
+++ mssql/run.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+name_container=sql_server
+function build {
+    docker build --no-cache -t $name_container $@ .
+}
+#        -P \
+#        -v $(pwd)/my_python:/app/my_python \
+function up {
+    docker run $@ \
+        --name $name_container \
+        -v $(pwd)/my_python:/app/my_python \
+        -p 9000:5432 \
+        -d $name_container
+
+}
+
+function down {
+    docker rm -f -v $name_container
+}
+function rebuild {
+    docker rm -f -v $name_container
+   docker run -e 'ACCEPT_EULA=Y' \
+   -e 'SA_PASSWORD=Post11234' \
+   --name $name_container \
+    -p 1433:1433  \
+    -d mcr.microsoft.com/mssql/server:2019-latest
+}
+function log {
+    docker logs $name_container $@
+}
+
+function sh {
+    docker exec $@ -it $name_container bash
+}
+function cron {
+    docker exec $@ -it $name_container bash /bin/build_project.sh
+}
+function del {
+  docker rmi -f $name_container
+}
+function show {
+  docker images $@ | grep $name_container
+}
+function start {
+  docker start $(docker ps -a -q -f "name=$name_container" --format "{{.ID}}") $@
+}
+function stop {
+  docker stop $(docker ps -f "name=$name_container" --format "{{.ID}}")
+}
+function main {
+    Command=$1
+    shift
+    case "${Command}" in
+        build)  build $@ ;;
+        up)     up $@ ;;
+        down)   down $@ ;;
+        log)    log $@ ;;
+        sh)     sh $@ ;;
+        del)     del $@ ;;
+        show)     show $@ ;;
+        cron)     cron $@ ;;
+        rebuild)     rebuild $@ ;;
+        start )     start $@ ;;
+        stop )     stop $@ ;;
+        *)      echo "Usage: $0 {build|up|down|log|sh|del}" ;;
+    esac
+}
+
+main $@
