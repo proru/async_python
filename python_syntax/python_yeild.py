2f7da9cf2d20a5ffe94f8b846f1febc166029b06
Shavkunov Konstantin <purposef@yandex.ru>
2021-08-02 19:48:16 +0500

    fetch(): add fetch syntax python

--- python_syntax/python_yeild.py
+++ python_syntax/python_yeild.py
@@ -1,5 +1,14 @@
 
 
+class Bank():
+    crisis = False
+
+    def create_atm(self):
+        while not self.crisis:
+            yield "$100"
+
+
+
 def function():
     mygenerator = (x*x for x in range(3))
     for i in mygenerator:
@@ -8,11 +17,22 @@ def function():
 
 def createGenerator() :
     mylist = range(3)
-    for i in mylist :
+    for i in mylist:
         yield i*i
 
 if __name__=="__main__":
-    print(function())
-    my_generator = createGenerator()
-    for i in my_generator:
-        print(i)
+    # print(function())
+    # my_generator = createGenerator()
+    # for i in my_generator:
+    #     print(i)
+    bank = Bank()
+    temp_atm = bank.create_atm()
+    print(next(temp_atm))
+    print([next(temp_atm) for _ in range(5)])
+    bank.crisis = True
+    # print(next(temp_atm))
+    atm_two = bank.create_atm()
+    # print(next(atm_two))
+    bank.crisis = False
+    atm_three = bank.create_atm()
+    print(next(atm_three))
